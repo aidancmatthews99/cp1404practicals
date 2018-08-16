05d2bb87b39434f83d31ea0393f80ad85de8899d
aidancmatthews99 <aidan.matthews@my.jcu.edu.au>
2018-08-16 21:10:51 +1000

    Refactor password_entry to use functions

---
+++ prac_3/password_entry.py
@@ -0,0 +1,22 @@
+"""Aidan"""
+
+def main():
+    MIN_LENGTH = 6
+    password = get_password()
+
+    while len(password) < MIN_LENGTH:
+        print("Error! Password too short!")
+        password = get_password()
+
+    print_asterix(password)
+
+
+def print_asterix(password):
+    print("*" * len(password))
+
+
+def get_password():
+    return input("Password: ")
+
+
+main()
