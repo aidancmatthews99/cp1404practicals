324b64c45dfe45d67db78e754f3603f8b8e5301f
aidancmatthews99 <aidan.matthews@my.jcu.edu.au>
2018-08-30 15:10:10 +1000

    Add all of prac_04

--- prac_04/lottery_ticket_generator.py
+++ prac_04/lottery_ticket_generator.py
@@ -4,13 +4,14 @@ import random
 number_picks = int(input("How many 'Quick Picks'? "))
 picks = []
 random_number = random.randint(0, 45)
+for i in range(0, number_picks):
+    j = 0
+    picks.append([])
+    while j < 6:
+        if random_number not in picks[i]:
+            picks[i].append(random_number)
+            j += 1
+        random_number = random.randint(0, 45)
 
-picks = []
-
-# for j in range(0, number_picks):
-#     for i in range(0, 6):
-#         while random_number in picks:
-#             random_number = random.randint(0, 45)
-#         picks.append(random_number)
 
 print(picks)
